c9db5d2add2bd027b7d5fb25b91fbfb41f816759
jesperstig <36713489+jesperstig@users.noreply.github.com>
2021-05-24 22:40:42 +0200

    test til webserver

--- crm.php
+++ crm.php
@@ -28,22 +28,23 @@ $currentComp = 'virksomhed1';
 </style>
 
 <div class="searchbar-custom container-fluid">
-    <div class="row">
-        <div class="col-5  my-auto">
+    <div class="row mt-2 mb-2">
+        <div class="col-sm-5 ">
+            <!--
             <div class="form-group"> 
                 <label for="customer-input">Kunde</label>
                 <input class= "form-control" id="customer-input" type="text">
             </div>
-            <div class="form-group">
-                <label for="cvr-input">CVR</label>
-            
-                <input class="form-control" id="cvr-input" type="text">
+            -->
+            <div class="form-group my-auto">
+                <!--<label for="cvr-input">CVR</label>-->
+                <abbr title="Indtast CVR på kunden du vil slå op"><input class="form-control" id="cvr-input" type="text" placeholder="CVR:"></abbr>
             </div>
         </div>
-        <div class="col-2 my-auto mx-auto">
+        <div class="col-sm-2 my-sm-auto mx-auto mt-1">
             <button class="btn btn-block">Søg</button>
         </div>
-        <div class="col-5 my-auto">
+        <div class="col-sm-5 my-sm-auto mt-1">
             <button class="btn btn-block ">Opret ny kunde</button>
         </div>
     </div>
@@ -51,102 +52,136 @@ $currentComp = 'virksomhed1';
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col">
+        <div class="col" style="">
             <div class="row">
-                <div class="col">
+                <div class="col" style="border:2px solid black;">
                     Primær kontaktperson:
-                    <br>
-                    Navn 1
-                    <br>
-                    Sekundær kontaktperson 
-                    <br>
-                    Navn 2
-                    <br>
-                    Navn 3
+                    <ul class="list-unstyled">
+                        <li>Navn 1</li>
+                    </ul>
+                    
+                    Sekundær(e) kontaktperson(er)
+                    <ul class="list-unstyled">
+                        <li>
+                            <div class="row">
+                                <div class="col-auto" style="border:1px solid deeppink;">
+                                    <img src="img\users\modulopgave 3 portrætter 1.png" alt="" style="width:40px;height:40px;">
+                                </div>
+                                <div class="col" style="border:1px solid deeppink;">
+                                    <div class="row">
+                                        <div class="col">
+                                            <span class="small">Navn <br> Kunde</span>
+                                            <span class="x-small"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col">
+                                            
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <span class="small">Email</span> 
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <span class="small">Tlf</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                        
+                        </li>
+                        <li>Navn 3</li>
+                    </ul >
                 </div>
             </div>
             <div class="row">
-                <div class="col" style="border:4px dotted black;">
+                <div class="col" style="border:2px solid black;">
                     <?php
-                    //print_r(dirToArray('files'));
-                    //echo count(dirToArray('files'));
                     foreach (dirToArray($rootFiles) as $key => $value) {
-                        //echo $key;
-                        
-                        if(!is_dir("files/" . $key)) { ?>
-                            <div class="col file-custom lvl1-custom">
-                                <img src='img/system/file.png' class="icon-custom"><?php echo $value; ?>
-                            </div> <?php
-                            
-                        } else{ ?>
-                            <div class="col folder-custom lvl1-custom">
-                                <img src='img/system/folder.png' class="icon-custom"><?php echo $key; ?>
-                            </div> 
-                            <?php $lvl2Files = $rootFiles . $key . "/";
-                            foreach (dirToArray($lvl2Files) as $key => $value) {
-                                if(!is_dir($lvl2Files . $key)) { ?>
-                                    <div class="col file-custom lvl2-custom">
-                                        <img src='img/system/file.png' class="icon-custom ml-4"><?php echo $value;?>
-                                    </div>
-                                    
-                                <?php } else {?>
-                                    <div class="col folder-custom lvl2-custom">
-                                        <img src='img/system/folder.png' class="icon-custom ml-4"><?php echo $key; ?>
-                                    </div> <?php
-                                    $lvl3Files = $lvl2Files . $key . "/";
-                                    foreach (dirToArray($lvl3Files) as $key => $value) {
-                                        if(!is_dir($lvl3Files . $key)) { ?>
-                                            <div class="col file-custom lvl3-custom">
-                                                <img src='img/system/file.png' class="icon-custom ml-5"><?php echo $value;?> 
-                                            </div>
-                                            
-                                        <?php } else{ ?>
-                                            <div class="col folder-custom lvl3-custom">
-                                                <img src='img/system/folder.png' class="icon-custom ml-5"><?php echo $key; ?>
-                                            </div>
-                                            
-                                        <?php
+                        //if ($key == //client_id[searched ID]) {                        
+                            if(!is_dir("files/" . $key)) { ?>
+                                <div class="col file-custom lvl1-custom">
+                                    <img src='img/system/file.png' class="icon-custom"><?php echo $value; ?>
+                                </div> <?php
+                                
+                            } else{ ?>
+                                <div class="col folder-custom lvl1-custom">
+                                    <img src='img/system/folder.png' class="icon-custom"><?php echo $key; ?>
+                                </div> 
+                                <?php $lvl2Files = $rootFiles . $key . "/";
+                                foreach (dirToArray($lvl2Files) as $key => $value) {
+                                    if(!is_dir($lvl2Files . $key)) { ?>
+                                        <div class="col file-custom lvl2-custom">
+                                            <img src='img/system/file.png' class="icon-custom ml-4"><?php echo $value;?>
+                                        </div>
+                                        
+                                    <?php } else {?>
+                                        <div class="col folder-custom lvl2-custom">
+                                            <img src='img/system/folder.png' class="icon-custom ml-4"><?php echo $key; ?>
+                                        </div> <?php
+                                        $lvl3Files = $lvl2Files . $key . "/";
+                                        foreach (dirToArray($lvl3Files) as $key => $value) {
+                                            if(!is_dir($lvl3Files . $key)) { ?>
+                                                <div class="col file-custom lvl3-custom">
+                                                    <img src='img/system/file.png' class="icon-custom ml-5"><?php echo $value;?> 
+                                                </div>
+                                                
+                                            <?php } else{ ?>
+                                                <!--
+                                                <div class="col folder-custom lvl3-custom">
+                                                    <img src='img/system/folder.png' class="icon-custom ml-5"><?php echo $key; ?>
+                                                </div>
+                                                -->
+                                                
+                                            <?php
+                                            }
                                         }
                                     }
                                 }
                             }
-                        }
+                        //}
                     }?>
                 </div>
             </div>
         </div>
         <div class="col">
             <div class="row">
-                <div class="col">
-                    Ejer:
+                <div class="col" style="border:2px solid black;">
+                    Ejer(e):
                     <br> 
-                    Navn 4
+                    <ul class="list-unstyled">
+                        <li>Navn 4</li>
+                    </ul>
+                    
                 </div>
             </div>
             <div class="row">
-                <div class="col">
-                    Virksomhedsinformationer
-                    <br>
-                    Aktiv kunde: ja/nej 
-                    <br>
-                    Kontaktudløb: 
-                    <br>
-                    Deadline: N/A/31-02-22 
-                    <br>
-                    Regnskabsår:01-01-31-12 
-                    <br>
-                    Omsætning af de seneste 5 år 
+                <div class="col" style="border:2px solid black;">
+                    <h2>Virksomhedsinformationer</h2>
+                    <ul class="list-unstyled">
+                        <li>Aktiv kunde: ja/nej</li>
+                        <li>Kontaktudløb:</li>
+                        <li>Deadline: N/A/31-02-22 </li>
+                        <li>Regnskabsår:01-01-31-12 </li>
+                    </ul>
+                     
+                    
+                    
             
-                    <div class="container">
+                    <div class="container" style="border:2px solid black;">
+                        Omsætning af de seneste 5 år
                         <table class="table">
-
                             <thead>
                                 <tr>    
-                                    <th>2016</th>
-                                    <th>2017</th>
-                                    <th>2018</th>
-                                    <th>2019</th>
-                                    <th>2020</th>
+                                    <th><?php echo date("Y") - 5 ?></th>
+                                    <th><?php echo date("Y") - 4 ?></th>
+                                    <th><?php echo date("Y") - 3 ?></th>
+                                    <th><?php echo date("Y") - 2 ?></th>
+                                    <th><?php echo date("Y") - 1 ?></th>
                                 </tr>
                             </thead>
                             <tbody>
