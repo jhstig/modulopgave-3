5b90a95886d256b2cff1b2afa63689381b5e644b
vemme123 <v.vemmelund@gmail.com>
2021-05-27 16:23:50 +0200

    fil system virker

--- crm.php
+++ crm.php
@@ -39,16 +39,16 @@ if(isset($_POST['cvr-input']) && !empty($_POST['cvr-input']) ){
     }
     .test{
         border: 1px solid black;
-    } 
+    }
     .searchbar-custom{
         border:1px solid black;
-        
+
     }
     .contacts-custom{
         border:1px solid black;
     }
-    
-    
+
+
 </style>
 
 <div class="searchbar-custom container-fluid"> <!-- Søgefelt -->
@@ -70,7 +70,7 @@ if(isset($_POST['cvr-input']) && !empty($_POST['cvr-input']) ){
 
 <?php if(isset($_POST['cvr-input']) && !empty($_POST['cvr-input'])){ ?>
     <div class="container-fluid">
-        <div class="row"> 
+        <div class="row">
             <div class="col-md-6" style="">
                 <div class="row"> <!-- primære og sekundærer kontaktpersoner -->
                     <div class="col" style="border-right:2px solid black;">
@@ -107,49 +107,46 @@ if(isset($_POST['cvr-input']) && !empty($_POST['cvr-input']) ){
                         <span class="h4">Filer</span>
                         <?php
                         foreach (dirToArray($rootFiles) as $key => $value) {
-                            //if ($key == //client_id[searched ID]) {                        
-                                if(!is_dir("files/" . $key)) { ?>
-                                    <div class="col file-custom lvl1-custom">
-                                        <img src='img/system/file.png' class="icon-custom"><?php echo $value; ?>
-                                    </div> <?php
-                                    
-                                } else{ ?>
-                                    <div class="col folder-custom lvl1-custom">
-                                        <img src='img/system/folder.png' class="icon-custom"><?php echo $key; ?>
-                                    </div> 
-                                    <?php $lvl2Files = $rootFiles . $key . "/";
-                                    foreach (dirToArray($lvl2Files) as $key => $value) {
-                                        if(!is_dir($lvl2Files . $key)) { ?>
-                                            <div class="col file-custom lvl2-custom">
-                                                <img src='img/system/file.png' class="icon-custom ml-4"><?php echo $value;?>
-                                            </div>
-                                            
-                                        <?php } else {?>
-                                            <div class="col folder-custom lvl2-custom">
-                                                <img src='img/system/folder.png' class="icon-custom ml-4"><?php echo $key; ?>
-                                            </div> <?php
-                                            $lvl3Files = $lvl2Files . $key . "/";
-                                            foreach (dirToArray($lvl3Files) as $key => $value) {
-                                                if(!is_dir($lvl3Files . $key)) { ?>
-                                                    <div class="col file-custom lvl3-custom">
-                                                        <img src='img/system/file.png' class="icon-custom ml-5"><?php echo $value;?> 
-                                                    </div>
-                                                    
-                                                <?php } else{ ?>
-                                                    <!--
-                                                    <div class="col folder-custom lvl3-custom">
-                                                        <img src='img/system/folder.png' class="icon-custom ml-5"><?php echo $key; ?>
-                                                    </div>
-                                                    -->
-                                                    
-                                                <?php
+                                if(getFile($pkCompany == $key)){
+
+                                  echo $key;
+
+
+                                //if ($key == //client_id[searched ID]) {
+                                    if(!is_dir("files/" . $key)) { ?>
+                                        <div class="col file-custom lvl1-custom">
+                                            <img src='img/system/file.png' class="icon-custom"><?php echo getFile($value)[0]['filename']; ?>
+                                        </div> <?php
+
+                                    } else{ ?>
+                                        <div class="col folder-custom lvl1-custom">
+                                            <img src='img/system/folder.png' class="icon-custom"><?php echo getClient(getFile($key)[0]['client'])[0]['name']; ?>
+                                        </div>
+                                        <?php $lvl2Files = $rootFiles . $key . "/";
+                                        foreach (dirToArray($lvl2Files) as $key => $value) {
+                                            if(!is_dir($lvl2Files . $key)) { ?>
+                                                <div class="col file-custom lvl2-custom">
+                                                    <img src='img/system/file.png' class="icon-custom ml-4"><?php echo getFile($value)[0]['filename'];?>
+                                                </div>
+
+                                            <?php } else {?>
+                                                <div class="col folder-custom lvl2-custom">
+                                                    <img src='img/system/folder.png' class="icon-custom ml-4"><?php echo $key; ?>
+                                                </div> <?php
+                                                $lvl3Files = $lvl2Files . $key . "/";
+                                                foreach (dirToArray($lvl3Files) as $key => $value) {
+                                                    if(!is_dir($lvl3Files . $key)) { ?>
+                                                        <div class="col file-custom lvl3-custom">
+                                                            <img src='img/system/file.png' class="icon-custom ml-5"><?php echo getFile($value)[0]['filename'];;?>
+                                                        </div>
+                                                    <?php }
+                                                    //}
                                                 }
                                             }
                                         }
                                     }
                                 }
-                            //}
-                        }?>
+                            }?>
                     </div>
                 </div>
             </div>
@@ -158,14 +155,14 @@ if(isset($_POST['cvr-input']) && !empty($_POST['cvr-input']) ){
                     <div class="col" style="">
                         <?php include("components/owners.php") ?>
                         Ejer(e):
-                        <br> 
+                        <br>
                         <ul class="list-unstyled">
                             <?php
                                 for($i = 0; $i < count($owners); $i++) { //for hver owner, gør følgende:
                                     printEmployee($owners[$i]['user_id']);
                                 }
                             ?>
-                            
+
                             <?php/*
                                 foreach($owners as $value) {
                                     printEmployee($value);
@@ -182,7 +179,7 @@ if(isset($_POST['cvr-input']) && !empty($_POST['cvr-input']) ){
             </div>
         </div>
     </div>
-<?php 
+<?php
 }
 include('templates/footer.php')
 ?>
